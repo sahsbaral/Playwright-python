ea9e74b1f80b3164fb023c702fb9e6c898f0a726
Sami <samikshya.baral021@apexcollege.edu.np>
2026-09-03 10:25:04 +0545

    Verify all the prices from the products page

--- tests/test_products.py
+++ tests/test_products.py
@@ -43,10 +43,27 @@ def test_product_names(page:Page):
 
     #Now check the real names of the products displayed
     expected_products=["Sauce Labs Backpack","Sauce Labs Bike Light","Sauce Labs Bolt T-Shirt","Sauce Labs Fleece Jacket","Sauce Labs Onesie","Test.allTheThings() T-Shirt (Red)"]
-    products=page.locator(".inventory_item_name") #There is a diff between ".inventory_item" and ".inventory_item_name".
+    products=page.locator(".inventory_item_name") 
+    #There is a diff between ".inventory_item" and ".inventory_item_name".
     #The first one gives the whole card information while the second one gives Product Names specifically.
     assert products.all_text_contents() == expected_products
 
+#Verify all the correct prices are displayed
+def test_verify_prices(page:Page):
+    page.goto("https://www.saucedemo.com/")
+
+    page.get_by_placeholder("Username").fill("standard_user")
+    page.get_by_placeholder("Password").fill("secret_sauce")
+    page.get_by_role("button",name="Login").click()
+
+    expect(
+        page.get_by_text("Products")
+    ).to_be_visible()
+
+    expected_prices=["$29.99","$9.99","$15.99","$49.99","$7.99","$15.99"]
+    products=page.locator(".inventory_item_price")
+
+    assert products.all_text_contents() == expected_prices
 
 
 #Add the First Item to the Cart and Verify the Cart Badge
